f709a8fa473af98570f3067beee25fc7988e66eb
Wojciech Szemik <wojciech.szemik@grudnik.pl>
2023-04-05 14:18:35 +0200

    bug fixed with costs

--- src/Functions/getValueOfOptionalEquipment.js
+++ src/Functions/getValueOfOptionalEquipment.js
@@ -2,7 +2,9 @@ export const getValueOfOptionalEquipment = (list) => {
   let value = 0;
   list.forEach((unit) => {
     unit.optionalEquipment.forEach((eq) => {
-      if (eq != "Sygnalista" || eq != "Chorąży") {
+    
+      if (eq != "Sygnalista" && eq != "Chorąży") {
+        console.log(eq)
         value += unit.equipmentList[eq][0] * unit.selectedNumber;
       }
     });
