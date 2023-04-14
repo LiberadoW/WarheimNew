205ebdff65f8d0690ff117115076fa2f5093f760
LiberadoW <75096962+LiberadoW@users.noreply.github.com>
2023-04-14 22:10:50 +0200

    Update getModifiers.js

--- src/Functions/getModifiers.js
+++ src/Functions/getModifiers.js
@@ -83,7 +83,7 @@ export const getModifiers = (arr, unit) => {
     initativeModifier = 2;
   }
 
-  if (unit.rules.includes("Krzepki")) {
+  if (unit.rules.includes("Krzepki") || unit.rules.includes("Nieumarły") {
     speedModifier = 0;
     initativeModifier = 0;
   }
